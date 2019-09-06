1bfba8970de9fc33fb80123ffde0e52688f2e26d
Vineet Prasad Verma <vineetpd1996@gmail.com>
2019-09-06 22:58:10 +0530

    Learnt Basic Inbuilt methods(Alert, Prompt, Console) in JavaScript

---
+++ JAVASCRIPT/wrong.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your First Name ?");
+var lastName= prompt("What is your last name ?");
+var age = prompt("Your age ?");
+console.log("Your name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old.")
