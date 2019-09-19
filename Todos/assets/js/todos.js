a62daeac3991d77186530854cf30c4d8822a6ef3
Vineet Prasad Verma <vineetpd1996@gmail.com>
2019-09-19 10:37:16 +0530

    Add a functionality to remove a todo[C

--- Todos/assets/js/todos.js
+++ Todos/assets/js/todos.js
@@ -1,3 +1,12 @@
 $("li").click(function(){
     $(this).toggleClass("completed");
 });
+
+
+//to delete todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+            $(this).remove();
+    });
+    event.stopPropagation();
+});
