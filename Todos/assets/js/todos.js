243121f00c644bce0e720fcaebb0c34467be28cc
Vineet Prasad Verma <vineetpd1996@gmail.com>
2019-09-21 10:18:31 +0530

    Todo list to add, remove, marked completed with cool styling and designing

--- Todos/assets/js/todos.js
+++ Todos/assets/js/todos.js
@@ -1,12 +1,24 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
-
-
+ 
+ 
 //to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
             $(this).remove();
     });
     event.stopPropagation();
 });
+ 
+//to add todo
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span>" + $(this).val() + "</li>");
+    $(this).val("");
+    }
+});
+ 
+$("h1 i").click(function(){
+    $("input[type='text']").fadeToggle();
+}); 
